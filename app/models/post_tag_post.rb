ec4763f62243d26ff00163fec4ac95f2aba7be08
kawa-sho <syogo.wao@gmail.com>
2022-06-10 08:53:54 +0000

    Fix model post_tag_post

--- app/models/post_tag_post.rb
+++ app/models/post_tag_post.rb
@@ -2,8 +2,4 @@ class PostTagPost < ApplicationRecord
   ## アソシエーション
   belongs_to :post
   belongs_to :tag_post
-
-  ## バリデーション
-  #validates :post_id, presence: true
-  #validates :tag_post_id, presence: true
 end
