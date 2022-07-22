8d80b5f975eae374bdc4396dff4b7c62dc9b03d6
kawa-sho <syogo.wao@gmail.com>
2022-07-22 11:01:07 +0000

    update バッチ処理

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,7 +23,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = Rails.env.to_sym
 set :environment, rails_env
 set :output, 'log/cron.log'
-every 2.hour do
+every 12.hour do
   begin
     runner "Batch::DataReset.data_reset"
   rescue => e
